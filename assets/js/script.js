9a2716e7a935d580531ed1afb3fd70440951dc29
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-05-13 23:23:58 -0500

    Fixed issues with game restart. Reset event listeners by cloning body and swapping it with the clone

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,11 @@ function createBlanks (word, containerULElement) {
     }
 }
 
+function clearDisplay () {
+    document.getElementById("word-to-be-guessed").innerHTML = "";
+    document.getElementById("strikes").innerHTML = "";
+}
+
 /*  Create a function that runs the game. runGame expects a callback that will be executed
     once the dictionary data has been loaded from the remote server; that callback will
     actually execute the game logic.
@@ -38,15 +43,15 @@ function createBlanks (word, containerULElement) {
     https://www.w3schools.com/js/js_json_parse.asp
     https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
     https://gist.github.com/thiagodebastos/08ea551b97892d585f17 */
-function runGame (callback) {
+function loadGame (callback) {
     const xmlReq = new XMLHttpRequest();
     let progressContainer = document.getElementById("prog-container");
     xmlReq.overrideMimeType("application/json");
     xmlReq.open("GET", "https://raw.githubusercontent.com/matthewreagan/WebstersEnglishDictionary/master/dictionary.json", true);
     xmlReq.onreadystatechange = function () {
         if (xmlReq.readyState == 4 && xmlReq.status == "200") {
+            progressContainer.classList.add("d-none");
             callback(xmlReq.responseText);
-            progressContainer.classList.add("invisible");
         }
     }
     xmlReq.send(null);
@@ -68,6 +73,9 @@ function keyAppears (key, currentWord) {
             if (appearsIndex >= 0) {
                 appearsAt.push(appearsIndex);
                 i = appearsIndex;
+            }
+            else if (appearsIndex === -1) {
+                i = currentWord.length;
             }
         }
     }
@@ -113,61 +121,82 @@ function writeDefinition (definition) {
     document.getElementById("definition").innerHTML = definition;
 }
 
-window.onload = function () {
+function gameLogic(response) {
 
-    // Running game, callback function is anonymous function with game logic
-    runGame(function (response) {
+    // Declare game variables
+    
+    /* dictionaryFile is the parsed object from the remote JSON file given
+       by the parameter response (the xmlReq.responseText from runGame function) */
+    const dictionaryFile = JSON.parse(response);
+    /* gameDict is the Dictionary object from which we will pull words and definitions */
+    const gameDict = new Dictionary(dictionaryFile);
+    /* word will be the variable in which we store the word string to be guessed */
+    let word;
+    let usedKeys = [];
+    let strikes = 0;
+    
+    // Set the word to be guessed to a new word from the game dictionary
+    word = gameDict.newWord();
+    
+    // Write out the blanks (the lis that contain the letters made invisible)
+    createBlanks(word, document.getElementById("word-to-be-guessed"));
+    
+    // Write out the definition in p#definition
+    // do this on win or loss writeDefinition(gameDict.getDefinition(word));
 
-        // Declare game variables
-        
-        /* dictionaryFile is the parsed object from the remote JSON file given
-           by the parameter response (the xmlReq.responseText from runGame function) */
-        const dictionaryFile = JSON.parse(response);
-        /* gameDict is the Dictionary object from which we will pull words and definitions */
-        const gameDict = new Dictionary(dictionaryFile);
-        /* word will be the variable in which we store the word string to be guessed */
-        let word;
-        let usedKeys = [];
-        let strikes = 0;
-        
-        // Set the word to be guessed to a new word from the game dictionary
-        word = gameDict.newWord();
-        
-        // Write out the blanks (the lis that contain the letters made invisible)
-        createBlanks(word, document.getElementById("word-to-be-guessed"));
-        
-        // Write out the definition in p#definition
-        // do this on win or loss writeDefinition(gameDict.getDefinition(word));
-        
-        // Show helper; for debugging, delete for publishing
-        document.getElementById("showHideButton").addEventListener("click", function () {
-            showHideLetters(document.getElementById("word-to-be-guessed"));
-        });
+    /* Clone body to get rid of previous keyup event listeners */
+    /* Got this little hack that saved my game from 
+    https://stackoverflow.com/questions/19469881/remove-all-event-listeners-of-specific-type?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa */
+    let htmlBody = document.getElementById("pageBody"), bodyClone = htmlBody.cloneNode(true);
+    htmlBody.parentElement.replaceChild(bodyClone, htmlBody);
 
-        // Show bottom display helper; for debuggin, delete for publishing
-        document.getElementById("showBottom").addEventListener("click", function () {
-            if (document.getElementById("bottom-display").classList.contains("d-none")) {
-                revealBottomDisplay();
-            }
-            else {
-                hideBottomDisplay();
-            }
-        });
+    /* Putting all event listeners here in the logic so that they're reset when the game resets */
 
-        /* Create key listener that uses keyAppears to determine key appearance. 
-           If it appears, add the key to usedKeys and then reveal all instances
-           of the key. If not, increment strikes
-           
-           appearance is an object returned by keyAppears that contains whether or not the 
-           key appears as a boolean (appearance.appears) and an array of indices at which
-           the given key appears (appearance.indices)
-           */
-        document.addEventListener("keyup", function(event) {
-            let appearance = keyAppears(event.key, word);
-            if (appearance.appears) {
-                revealLetters(appearance.indices);
-            }
-        })
+    /* Create key listener that uses keyAppears to determine key appearance. 
+       If it appears, add the key to usedKeys and then reveal all instances
+       of the key. If not, increment strikes
+       
+       appearance is an object returned by keyAppears that contains whether or not the 
+       key appears as a boolean (appearance.appears) and an array of indices at which
+       the given key appears (appearance.indices)
+       */
+    document.body.addEventListener("keyup", function(event) {
+        let appearance = keyAppears(event.key, word);
+        if (appearance.appears) {
+            revealLetters(appearance.indices);
+        }
+        else {
+            strikes++;
+            document.getElementById("strikes").innerHTML = strikes;
+        }
     });
 
-};
+    document.getElementById("play-again").addEventListener("click", function () {
+        clearDisplay();
+        loadGame(gameLogic);
+    });
+    
+    // Show helper; for debugging, delete for publishing
+    document.getElementById("showHideButton").addEventListener("click", function () {
+        showHideLetters(document.getElementById("word-to-be-guessed"));
+    });
+    
+    // Show bottom display helper; for debuggin, delete for publishing
+    document.getElementById("showBottom").addEventListener("click", function () {
+        if (document.getElementById("bottom-display").classList.contains("d-none")) {
+            revealBottomDisplay();
+        }
+        else {
+            hideBottomDisplay();
+        }
+    });
+
+}
+
+window.onload = function () {
+
+    // Running game, callback function is gameLogic
+    clearDisplay();
+    loadGame(gameLogic);
+
+}
